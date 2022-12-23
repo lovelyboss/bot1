88df75778268b1afa1b2db13b457b5f395361236
lovelyboss <73265891+lovelyboss@users.noreply.github.com>
2022-12-23 12:14:08 +0530

    Update fltauto.js

--- tools/fltauto.js
+++ tools/fltauto.js
@@ -73,12 +73,12 @@ const { chalk, inquirer, _, fs, instagram, print, delay } = require("./index.js"
                         if (!media.has_liked && !media.user.is_private && !status.following && !status.followed_by) {
                             const text = inputMessage.split("|");
                             const msg = text[Math.floor(Math.random() * text.length)];
-                            const task = [ig.follow(media.user.pk), ig.like(media.pk), ig.comment(media.pk, msg)];
-                            let [follow, like, comment] = await Promise.all(task);
-                            follow = follow ? chalk.bold.green(`Follow`) : chalk.bold.red("Follow");
+                            const task = [ig.like(media.pk), ig.comment(media.pk, msg)];
+                            let [like, comment] = await Promise.all(task);
+                            
                             like = like ? chalk.bold.green("Like") : chalk.bold.red("Like");
                             comment = comment ? chalk.bold.green("Comment") : chalk.bold.red("Comment");
-                            print(`▲ @${media.user.username} ⇶ [${follow}, ${like}, ${comment}] ⇶ ${chalk.cyanBright(msg)}`);
+                            print(`▲ @${media.user.username} ⇶ [${like}, ${comment}] ⇶ ${chalk.cyanBright(msg)}`);
                         } else print(chalk`▼ @${media.user.username} ⇶ {yellow Media already liked or user already followed/follows you!}`);
                     })
                 );
