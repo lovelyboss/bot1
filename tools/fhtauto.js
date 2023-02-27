7ea36c8a58ef8b120ace4ca8847c0135416b2bfb
lovelyboss <73265891+lovelyboss@users.noreply.github.com>
2023-02-27 15:50:29 +0530

    Update fhtauto.js

--- tools/fhtauto.js
+++ tools/fhtauto.js
@@ -3,14 +3,17 @@ const { chalk, inquirer, _, fs, instagram, print, delay } = require("./index.js"
 (async () => {
     print(
         chalk`{bold.green
-  ▄▄▄▄▄            ▄▄▌  .▄▄ · ▪   ▄▄ • 
-  •██  ▪     ▪     ██•  ▐█ ▀. ██ ▐█ ▀ ▪
-   ▐█.▪ ▄█▀▄  ▄█▀▄ ██▪  ▄▀▀▀█▄▐█·▄█ ▀█▄
-   ▐█▌·▐█▌.▐▌▐█▌.▐▌▐█▌▐▌▐█▄▪▐█▐█▌▐█▄▪▐█
-   ▀▀▀  ▀█▄▀▪ ▀█▄▀▪.▀▀▀  ▀▀▀▀ ▀▀▀·▀▀▀▀ 
+          |
+    ______|______
+  /  ___________  \
+ |  |---|   |---|  |
+||        |        ||
+ |    \_______/    |
+  \_______________/
+  Ξ TITLE  : bot v5.0
+  Ξ EMAIL  : lovelyboss9828@gmail.com
+  Ξ UPDATE : sunday, December 3, 2021
 
-  Ξ TITLE  : Folow Like Comment (Hastag Target)
-  Ξ UPDATE : Wednesday, August 4, 2021 (GMT+8)
            : TESTED "OK" BUG? YouTellMe!
     }`
     );
@@ -73,13 +76,13 @@ const { chalk, inquirer, _, fs, instagram, print, delay } = require("./index.js"
                         if (!media.has_liked && !media.user.is_private && !status.following && !status.followed_by) {
                             const text = inputMessage.split("|");
                             const msg = text[Math.floor(Math.random() * text.length)];
-                            const task = [ig.follow(media.user.pk), ig.like(media.pk), ig.comment(media.pk, msg)];
-                            let [follow, like, comment] = await Promise.all(task);
-                            follow = follow ? chalk.bold.green(`Follow`) : chalk.bold.red("Follow");
+                            const task = [ig.like(media.pk), ig.comment(media.pk, msg)];
+                            let [like, comment] = await Promise.all(task);
+                            
                             like = like ? chalk.bold.green("Like") : chalk.bold.red("Like");
                             comment = comment ? chalk.bold.green("Comment") : chalk.bold.red("Comment");
-                            print(`▲ @${media.user.username} ⇶ [${follow}, ${like}, ${comment}] ⇶ ${chalk.cyanBright(msg)}`);
-                        } else print(chalk`▼ @${media.user.username} ⇶ {yellow Private or already liked/followed/follows you}`);
+                            print(`▲ @${media.user.username} ⇶ [${like}, ${comment}] ⇶ ${chalk.cyanBright(msg)}`);
+                        } else print(chalk`▼ @${media.user.username} ⇶ {yellow Private or already liked/follows you}`);
                     })
                 );
                 if (i < items.length - 1) print(`Current Account: (${login.username}) » Delay: ${perExec}/${delayTime}ms \n`, "wait", true);
